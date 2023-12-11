bb818a3baa91a5c5a9c12f3b4b7a1cfab506003f
peninah98 <peninangizwenayo@gmail.com>
2023-12-11 11:30:29 +0200

    faet: testing get end point

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  @Get('/tasks')
+  getTasks(): string {
+    return 'This is a list of tasks';
+  }
+}
