7cd5004834b119753be7273ee6988b07d9c18fcf
peninah98 <peninangizwenayo@gmail.com>
2023-12-11 19:11:22 +0200

    feat: genetaring files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './tasks/tasks.module';
 import { CategoriesModule } from './categories/categories.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TodoModule, CategoriesModule],
+  imports: [CategoriesModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
