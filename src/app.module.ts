803607484c580e878e767d6630d5724af73707e6
peninah98 <78850411+peninah98@users.noreply.github.com>
2023-12-11 18:02:39 +0200

    Revert "Ft/controllers"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './tasks/todo.module';
-import { CategoriesModule } from './categories/categories.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule, CategoriesModule],
+  imports: [TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
