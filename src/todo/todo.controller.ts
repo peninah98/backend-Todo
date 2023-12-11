0b30c840b739ad1da2581e67d6afea2c45697874
peninah98 <peninangizwenayo@gmail.com>
2023-12-11 11:47:27 +0200

    feat: creating the post task testing functionalities

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller()
 export class TodoController {
   @Get('/tasks')
-  getTasks(): string {
+  getAllTasks(): string {
     return 'This is a list of tasks';
+  }
+
+  @Get('/tasks/:id')
+  getOneTask(@Param('id') id: string) {
+    return `${id}`;
+  }
+  @Post()
+  createTask(@Body() body: string) {
+    return `I am the ${body}`;
   }
 }
