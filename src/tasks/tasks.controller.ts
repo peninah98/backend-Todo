28bf6661ebe70a0c22493f71465549e0f28492cd
peninah98 <peninangizwenayo@gmail.com>
2023-12-11 19:06:16 +0200

    refactor: deleting some files

--- src/tasks/tasks.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { TodoService } from './tasks.service';
-
-@Controller()
-export class TodoController {
-  constructor(private readonly todoService: TodoService) {}
-}
--- src/tasks/tasks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodoController } from './tasks.controller';
-import { TodoService } from './tasks.service';
-
-@Module({
-  controllers: [TodoController],
-  providers: [TodoService],
-})
-export class TodoModule {}
