771988ff008768e6c039605bccc0dc33d2f4336a
peninah98 <peninangizwenayo@gmail.com>
2023-12-12 16:41:51 +0200

    feat: adding the modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
+  imports: [CategoriesModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,7 +1,9 @@
 import { CreateTasksDto } from './dto/create.tasks.dto';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { db } from 'src/main';
+import { v4 as uuidv4 } from 'uuid';
 
+const generatedId = uuidv4();
 @Injectable()
 export class TasksRepository {
   async getTasks() {
@@ -14,7 +16,7 @@ export class TasksRepository {
 
   async createTasks(body: CreateTasksDto) {
     try {
-      return await db.push('/tasks[]', { body }, true);
+      return await db.push('/tasks[]', { id: generatedId, ...body }, true);
     } catch (error) {
       throw new InternalServerErrorException(
         "Something went wrong can't create tasks",
